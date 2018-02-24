75538413e57d56d84c93e9ce5aec0ba5b943ba04
KasiaZR <k.zatorska-radlinska@fancyfon.com>
2018-02-25 00:54:04 +0100

    java rebuild

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,115 +1,152 @@
 package com.example.android.quizapp;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
-    CheckBox checkBoxOne;
-    CheckBox checkBoxTwo;
-    CheckBox checkBoxThree;
-    CheckBox checkBoxFour;
-    CheckBox checkBoxFive;
-    CheckBox checkBoxSix;
+    int score;
+    int MAX_SCORE = 5;
+
+    CheckBox checkBoxTrue1;
+    CheckBox checkBoxTrue2;
+    CheckBox checkBoxFalse1;
+    CheckBox checkBoxFalse2;
+    CheckBox checkBoxTrue3;
+    CheckBox checkBoxFalse3;
     EditText editTextDucati;
+    RadioGroup radioGroupIsleOfMan;
     RadioButton radioButtonNo;
     EditText editTextMotoGuzzi;
+    RadioGroup radioGroupFirstMotorcycle;
     RadioButton radioButtonLech;
+
+    private static final String DUCATI = "ducati";
+    private static final String MOTOGUZZI = "motoguzzi";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeGuiElements();
+    }
 
-        checkBoxOne = findViewById(R.id.checkbox_one);
-        checkBoxTwo = findViewById(R.id.checkbox_two);
-        checkBoxThree = findViewById(R.id.checkbox_three);
-        checkBoxFour = findViewById(R.id.checkbox_four);
-        checkBoxFive = findViewById(R.id.checkbox_five);
-        checkBoxSix = findViewById(R.id.checkbox_six);
+
+    protected void initializeGuiElements() {
+
+        checkBoxTrue1 = findViewById(R.id.checkbox_one);
+        checkBoxTrue2 = findViewById(R.id.checkbox_two);
+        checkBoxFalse1 = findViewById(R.id.checkbox_three);
+        checkBoxFalse2 = findViewById(R.id.checkbox_four);
+        checkBoxTrue3 = findViewById(R.id.checkbox_five);
+        checkBoxFalse3 = findViewById(R.id.checkbox_six);
         editTextDucati = findViewById(R.id.second_ducati_question);
+        radioGroupIsleOfMan = findViewById(R.id.radio_group_isle_of_man);
         radioButtonNo = findViewById(R.id.no_radio_button);
         editTextMotoGuzzi = findViewById(R.id.fourth_guzzi_question);
+        radioGroupFirstMotorcycle = findViewById(R.id.radio_group_first_motorcycle);
         radioButtonLech = findViewById(R.id.lech_radio_button);
+
     }
 
-    //Method used when button Submit is clicked
+    private void resetScore() {
 
-    public void Submit(View view) {
+        checkBoxTrue1.setChecked(false);
+        checkBoxTrue2.setChecked(false);
+        checkBoxTrue3.setChecked(false);
+        checkBoxFalse1.setChecked(false);
+        checkBoxFalse2.setChecked(false);
+        checkBoxFalse3.setChecked(false);
+        radioGroupFirstMotorcycle.clearCheck();
+        radioGroupIsleOfMan.clearCheck();
+        editTextDucati.setText(null);
+        editTextMotoGuzzi.setText(null);
+    }
 
-        //1st answer contains 3 incorrect answers (3rd,4th,6th), if they selected 0 point to score
-        if (checkBoxThree.isChecked() || (checkBoxFour.isChecked()) || (checkBoxSix.isChecked())) {
+
+    public void submit(final View view) throws InterruptedException {
+
+        /**
+         * 1 st answer contains 3 incorrect answers (3rd,4th,6th), if they selected 0 point to score
+         */
+
+        if (checkBoxFalse1.isChecked() || (checkBoxFalse2.isChecked()) || (checkBoxFalse3.isChecked())) {
             score += 0;
 
-            //1st answer contains 3 correct answers (1st,2nd,5th), if they selected +1 point to score
+            /**
+             * 1 st answer contains 3 correct answers (1st,2nd,5th), if they selected +1 point to score
+             */
 
-        } else if (checkBoxOne.isChecked() && (checkBoxTwo.isChecked()) && (checkBoxFive.isChecked())) {
-            score += 1;
+
+        } else if (checkBoxTrue1.isChecked() && (checkBoxTrue2.isChecked()) && (checkBoxTrue3.isChecked())) {
+            score++;
         }
 
-        //2nd answer is 'Ducati', if it's correct +1 point to score
-        if (editTextDucati.getText().toString().equalsIgnoreCase("ducati")) {
-            score += 1;
+        /**
+         * 2 nd answer is 'Ducati', if it's correct +1 point to score
+         */
+
+        if ("ducati".equalsIgnoreCase(DUCATI)) {
+            score++;
         }
 
-        // 3rd answer is 'No', if it's correct +1 point to score
+        /**
+         * 3 rd answer is 'No', if it's correct +1 point to score
+         */
+
         if (radioButtonNo.isChecked()) {
-            score += 1;
+            score++;
         }
 
-        // 4th answer is Moto Guzzi, if it's correct +1 point to score
+        /**
+         * 4 th answer is Moto Guzzi, if it's correct +1 point to score
+         */
 
-        if (editTextMotoGuzzi.getText().toString().equalsIgnoreCase("moto guzzi")) {
-            score += 1;
+
+        if ("motoguzzi".equalsIgnoreCase(MOTOGUZZI)) {
+            score++;
         }
 
-        //5th aswer is 'Lech', if it's correct +1 point to score
-        // 3rd answer is 'No', if it's correct +1 point to score
+        /***5th aswer is 'Lech', if it's correct +1 point to score
+         * 3rd answer is 'No', if it's correct +1 point to score
+         */
+
         if (radioButtonLech.isChecked()) {
-            score += 1;
+            score++;
         }
 
-        if (score == 5) {
-            //If someone get maximum points, display a toast message
-            Context context = getApplicationContext();
-            CharSequence text = "You scored " + score + "/5 \nCongratulation, you're 100% a motorcyclist!";
-            int duration = Toast.LENGTH_LONG;
 
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+        final String resultMaxScore = "Your score is " + MAX_SCORE + "/5\nCongratulations, you're a 100% motocyclist!";
+        final String resultBelowScore = "You scored " + score + "/5\nIt was close. Try again!";
 
+        /***
+         * If someone get maximum points, display a toast message
+         */
+        if (score == MAX_SCORE) {
+            Toast.makeText(this, resultMaxScore, Toast.LENGTH_LONG).show();
+
+            Thread.sleep(3000);
+            resetScore();
+
+            /***
+             * If someone get <5 points, display a toast message
+             */
+
+        } else if (score < 5) {
+                Toast.makeText(this, resultBelowScore, Toast.LENGTH_LONG).show();
+
+            Thread.sleep(3000);
+            resetScore();
         }
 
-        if (score < 5) {
-            //If someone get <5 points, display a toast message
-            Context context = getApplicationContext();
-            CharSequence text = "You scored " + score + "/5 \nIt was close. Try again!";
-            int duration = Toast.LENGTH_LONG;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
-        }
-
-        // New activity with 3 seconds of delay
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = getIntent();
-                finish();
-                startActivity(intent);
-            }
-        }, 3000);
 
     }
+
 }
